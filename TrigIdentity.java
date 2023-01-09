6622d6b54fb2c76d3c2d77b784c291a2faeec98d
colton <adamscolton64@gmail.com>
2023-01-09 12:34:34 -0600

    starting point

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -1,5 +1,11 @@
 public class TrigIdentity {
     public static void main(String[] args) {
-        
+        try {
+            Double i1 = Double.parseDouble(args[0]);
+            System.out.println(Math.sin(i1) + Math.cos(i1));
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Try again");
+        }
     }
 }
