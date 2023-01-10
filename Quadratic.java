e29cee9c40086394661b66974ef5b3ea49704282
colton <adamscolton64@gmail.com>
2023-01-10 11:34:14 -0600

    quadratic complete

--- Quadratic.java
+++ Quadratic.java
@@ -1,5 +1,20 @@
 public class Quadratic {
     public static void main(String[] args) {
-        
+        try {
+            double a = Integer.parseInt(args[0]);
+            double b = Integer.parseInt(args[1]);
+            double c = Integer.parseInt(args[2]);
+            double d = -b / (2*a);
+            double e = Math.sqrt(Math.pow(b, 2) - (4*a*c)) / (2*a);
+            System.out.println();
+            System.out.println(d + e);
+            System.out.println(d - e);
+            System.out.println();
+            System.out.println(d);
+            System.out.println(e);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("try again");
+        }
     }
 }
