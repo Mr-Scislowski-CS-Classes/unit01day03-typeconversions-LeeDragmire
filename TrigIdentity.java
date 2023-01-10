3123f0baf8fdd7a93b70677d161c9a3c33ce1d7d
colton <adamscolton64@gmail.com>
2023-01-10 11:19:56 -0600

    trigidentity complete

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -2,7 +2,7 @@ public class TrigIdentity {
     public static void main(String[] args) {
         try {
             Double i1 = Double.parseDouble(args[0]);
-            System.out.println(Math.sin(i1) + Math.cos(i1));
+            System.out.println(Math.pow(Math.sin(i1), 2) + Math.pow(Math.cos(i1), 2));
         }
         catch (NumberFormatException e) {
             System.out.println("Try again");
